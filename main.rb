e83a0639acffd2882d34ae59be0eba653f0421fa
jordy15 <jordyferdian88@gmail.com>
2021-09-14 19:14:08 +0700

    move knight_moves method to Knight class

--- main.rb
+++ main.rb
@@ -15,23 +15,6 @@ class Knight
       end
     end
   end
-end
-
-class ChessBoard
-  attr_reader :knight, :board
-  def initialize
-    @board = [
-      ['8a', '8b', '8c', '8d', '8e', '8f', '8g', '8h'],
-      ['7a', '7b', '7c', '7d', '7e', '7f', '7g', '7h'],
-      ['6a', '6b', '6c', '6d', '6e', '6f', '6g', '6h'],
-      ['5a', '5b', '5c', '5d', '5e', '5f', '5g', '5h'],
-      ['4a', '4b', '4c', '4d', '4e', '4f', '4g', '4h'],
-      ['3a', '3b', '3c', '3d', '3e', '3f', '3g', '3h'],
-      ['2a', '2b', '2c', '2d', '2e', '2f', '2g', '2h'],
-      ['1a', '1b', '1c', '1d', '1e', '1f', '1g', '1h'],
-    ]
-    @knight = Knight.new @board
-  end
 
   def knight_moves start, goal
     queue = [start]
@@ -42,7 +25,7 @@ class ChessBoard
 
     until queue.empty?
       current = queue[0]
-      @knight.move_graph[current[0]][current[1]].select {|move| distances[move[0]][move[1]] == -1}
+      @move_graph[current[0]][current[1]].select {|move| distances[move[0]][move[1]] == -1}
         .each do |move|
           paths[move[0]][move[1]] = current
           queue.push move
@@ -68,13 +51,30 @@ class ChessBoard
     movement = distances[row][col]
 
     puts "You made it in #{movement} moves! Here's your path: "
-    history.each {|steps| print "#{steps} #{@board[steps[0]][steps[1]]}\n"}
+    history.each {|steps| print "#{steps}\n"}
+  end
+end
+
+class ChessBoard
+  attr_reader :knight
+  def initialize
+    @board = [
+      ['8a', '8b', '8c', '8d', '8e', '8f', '8g', '8h'],
+      ['7a', '7b', '7c', '7d', '7e', '7f', '7g', '7h'],
+      ['6a', '6b', '6c', '6d', '6e', '6f', '6g', '6h'],
+      ['5a', '5b', '5c', '5d', '5e', '5f', '5g', '5h'],
+      ['4a', '4b', '4c', '4d', '4e', '4f', '4g', '4h'],
+      ['3a', '3b', '3c', '3d', '3e', '3f', '3g', '3h'],
+      ['2a', '2b', '2c', '2d', '2e', '2f', '2g', '2h'],
+      ['1a', '1b', '1c', '1d', '1e', '1f', '1g', '1h'],
+    ]
+    @knight = Knight.new @board
   end
 end
 
 def main 
   chessboard = ChessBoard.new
-  chessboard.knight_moves [3,3], [4,3]
+  chessboard.knight.knight_moves [0,0], [7,7]
 end
 
 main
